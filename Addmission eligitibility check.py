91c886fad7f8235a82e8606d5115ff3331728a77
gppirtakali <nishigandha261@gmail.com>
2026-06-09 00:52:49 +0530

    #Addmission eligibility check

---
+++ Addmission eligitibility check.py
@@ -0,0 +1,8 @@
+#Addmission eligibility check
+name=input("Enter the student name: ")
+perc=float(input("Enter the percent of student: "))
+if perc>=70 and perc<=100:
+    print("Student is eligible for addmission")
+
+else:
+    print("Student is not eligible for addmission")
