d8443e1a2333e616ce0109239fed4d5244304e98
gppirtakali <nishigandha261@gmail.com>
2026-06-09 01:25:36 +0530

    List Reference Comparison

---
+++ List Reference Comparison.py
@@ -0,0 +1,8 @@
+#this is List Reference Comparison system
+list1 = [10, 20, 30,40,50,50,60]
+list2 = list1
+
+if list1 is list2:
+    print("Both lists have same reference")
+else:
+    print("Different references")
