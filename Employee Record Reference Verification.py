ab83b5b61721d35c99befbc198cff195dbef91f8
gppirtakali <nishigandha261@gmail.com>
2026-06-09 01:32:04 +0530

    Employee Record Reference Verification

---
+++ Employee Record Reference Verification.py
@@ -0,0 +1,9 @@
+#this is Employee Record Reference Verification system
+
+employee1 = {"id":261, "name":"Sanket"}
+employee2 = employee1
+
+if employee1 is employee2:
+    print("Same employee record")
+else:
+    print("Different employee records")
