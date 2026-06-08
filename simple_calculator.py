a5423b0208de14e56066fcd35d848c15d39afd86
gppirtakali <nishigandha261@gmail.com>
2026-06-08 21:22:49 +0530

    simple calculator program

---
+++ simple_calculator.py
@@ -0,0 +1,15 @@
+#Simple calculator program
+
+number1=int(input("Enter a number"))
+number2=int(input("Enter another number"))
+
+#arithmetic operations
+
+print("*********************************************************************************************")
+print(f"Addition of {number1} and {number2} is",number1+number2)
+print(f"Subtraction of {number1} and {number2} is",number1 - number2)
+print(f"Multiplication of {number1} and {number2} is",number1*number2)
+print(f"Division of {number1} and {number2} is ",number1/number2)
+print(f"Modulo of {number1} and {number2} is",number1%number2)
+print(f"Exponent of {number1} and {number2} is",number1**number2)
+print("*********************************************************************************************")
