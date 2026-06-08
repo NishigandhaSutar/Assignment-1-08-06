3464284bd837a6d09717cae2dd2192e73ac15c81
gppirtakali <nishigandha261@gmail.com>
2026-06-09 00:56:14 +0530

    Scholarship eligibility check

---
+++ sholarship eligibility.py
@@ -0,0 +1,8 @@
+##Scholarship eligibility check
+name=input("Enter the student name: ")
+perc=float(input("Enter the percent of student: "))
+if perc>94:
+    print("Student is eligible for Scholarship")
+
+else:
+    print("Student is not eligible for Scholarship")
